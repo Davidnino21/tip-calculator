443cfb62fb67572a2e4c2109cb79119ff61a4ee9
Davidnino21 <davidnino_21@yahoo.com>
2025-03-19 14:10:42 -0700

    updated js file and html file

---
+++ index.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("button")
+const bill= document.getElementById("bill")
+const tip= document.getElementById("tip")
+const total= document.getElementById("total")
+
+btn.addEventListener('click', function () {
+    console.log(btn)
+})
